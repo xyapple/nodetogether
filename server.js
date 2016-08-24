54d934d423503357ca79d6d43c09fc69ac86610b
xyapple <ydeascentis@gmail.com>
2016-08-23 21:06:45 -0400

    create server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-require('express');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || '8080';
+const host = process.env.HOST ||'0.0.0.0';
+
+app.listen(port, host);
+
+console.log('Server running %s:%d...', host, port)
+
+
+
