4fba9a9ef700c2825b17c5fb0ad0d90230a7814c
xyapple <ydeascentis@gmail.com>
2016-08-23 23:30:41 -0400

    add a template

--- app/index.js
+++ app/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
 var motivations = require('motivations');
+const pickOne = require('pick-one');
+const exphbs = require('express-handlebars');
 
+app.use(express.static('./app/pubilc'));
 
-app.get("/", function(request, response) {
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  layoutsDir:'./app/views/layouts'
+}));
 
-  response.send (motivations);
+app.set('views', './app/views');
+app.set('view engine', 'handlebars');
 
+app.get('/', function(request, response) {
+
+  var motivation = pickOne(motivations);
+
+  response.render('motivation', { motivation: motivation });
 });
 
 module.exports = app;
