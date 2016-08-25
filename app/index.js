df6aaa4b0366c4ee59d9b4458bd06ecc97bb88b0
xyapple <ydeascentis@gmail.com>
2016-08-25 15:41:58 -0400

    add picture background donot work

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const app = express();
 
-app.use(express.static('./app/public'));
+app.use(express.static('./app/pubilc'));
 
 app.engine('handlebars', exphbs({
   defaultLayout: 'main',
@@ -16,11 +16,11 @@ const pickOne = require('pick-one');
 const motivations = require('motivations');
 const natImages = [1, 2, 3, 4, 5, 6];
 
-
 app.get('/', function(request, response) {
   var motivation = pickOne(motivations);
   var image = pickOne(natImages);
-  response.render('motivation', { motivation, image });
+
+  response.render('motivation', { motivation: image });
 });
 
 module.exports = app;
