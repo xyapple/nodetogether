b94f6a30f21f94e90b568c5779a66115163da139
xyapple <ydeascentis@gmail.com>
2016-08-29 11:49:10 -0400

    change the index.js code

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const exphbs  = require('express-handlebars');
 const app = express();
 
-app.use(express.static('./app/pubilc'));
+app.use(express.static('./app/public'));
 
 app.engine('handlebars', exphbs({
   defaultLayout: 'main',
@@ -14,13 +14,13 @@ app.set('view engine', 'handlebars');
 
 const pickOne = require('pick-one');
 const motivations = require('motivations');
-const natImages = [1, 2, 3, 4, 5, 6];
+const catImages = [1, 2, 3, 4, 5, 6];
+
 
 app.get('/', function(request, response) {
   var motivation = pickOne(motivations);
-  var image = pickOne(natImages);
-
-  response.render('motivation', { motivation: image });
+  var image = pickOne(catImages);
+  response.render('motivation', { motivation, image });
 });
 
 module.exports = app;
